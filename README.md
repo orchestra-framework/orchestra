df147e664a54819194331e2355c80e4e5c03e0c6
Fabrizio Armango <fabrizio.armango@gmail.com>
2021-09-08 14:31:45 +0200

    start parser via go api

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # orchestra
 Framework for developing digital and innovative documentations for software projects.
 
-Codify the knowledge needed in software development processes into powerful graph.
+Codify the knowledge needed in software development processes into powerful graphs.
 
 Code documentations and make complex queries.
 
--- services/orchestra-api/main.go
+++ services/orchestra-api/main.go
@@ -4,16 +4,38 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
+	"os/exec"
 )
 
 func main() {
 	router := gin.Default()
 
 	router.GET("/check", func(c *gin.Context) {
+		
 		c.JSON(200, gin.H{
 			"message": "server is working",
 		})
 	})
+
+	router.GET("/show", func(c *gin.Context) {
+		log.Print("here: show")
+	})
+
+	router.POST("/add", func(c *gin.Context) {
+
+		// get post body content and save it into file.
+		inputFileName := "tmp.orchx"	
+
+		c := exec.Command("cd /orchestra-parser;", "poetry", "run", "orchx2json", inputFileName)
+
+		if err := c.Run(); err != nil { 
+			log.Print("Error: ", err)
+		}   
+
+		log.Print("here: add")
+	})
+
+	
 
 	log.Print("Listening for requests in :5000")
 	router.Run(":5000")
